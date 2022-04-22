572c347de689147d334fa350d4ec7efbff5fe8a9
Ranjit <r.singhnagi@everdreamsoft.com>
2022-04-22 10:27:41 +0200

    Bug fix for address

--- src/canonizer/Binance/BinanceBlockchain.js
+++ src/canonizer/Binance/BinanceBlockchain.js
@@ -6,7 +6,7 @@ class BinanceBlockchain extends Blockchain_js_1.Blockchain {
     constructor(sandra) {
         super(sandra, "binance");
         this.addressFactory.is_a = 'bscAddress';
-        this.addressFactory.contained_in_file = 'blockchainAddressFile';
+        this.addressFactory.contained_in_file = 'bscAddressFile';
         this.addressFactory.onBlockchain = this.name;
         this.contractFactory.is_a = 'bscContract';
         this.contractFactory.contained_in_file = 'blockchainContractFile';
--- src/canonizer/Binance/BinanceBlockchain.ts
+++ src/canonizer/Binance/BinanceBlockchain.ts
@@ -8,7 +8,7 @@ export class BinanceBlockchain extends Blockchain {
         super(sandra, "binance");
 
         this.addressFactory.is_a = 'bscAddress';
-        this.addressFactory.contained_in_file = 'blockchainAddressFile';
+        this.addressFactory.contained_in_file = 'bscAddressFile';
         this.addressFactory.onBlockchain = this.name;
 
         this.contractFactory.is_a = 'bscContract';
